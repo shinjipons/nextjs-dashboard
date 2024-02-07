bbfb93ad017ced0ffde2633a7cf29b66578410e3
Shinji Pons <accounts@shinjipons.com>
2024-02-07 16:02:12 +0100

    End of tutorial

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,8 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
-import { signIn } from 'next-auth/react';
+import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
@@ -108,22 +107,21 @@ export async function deleteInvoice(id: string) {
     }
 }
 
-export async function authentificate(
+export async function authenticate(
     prevState: string | undefined,
     formData: FormData,
 ) {
     try {
-        await signIn('credentials', formData)
+        await signIn('credentials', formData);
     } catch (error) {
         if (error instanceof AuthError) {
             switch (error.type) {
                 case 'CredentialsSignin':
-                    return 'Invalid credentials.';
+                    return 'Invalid Credentials.';
                 default:
                     return 'Something went wrong.';
             }
         }
         throw error;
     }
-
 }
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -9,10 +9,10 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useFormState, useFormStatus } from 'react-dom';
-import { authentificate } from '../lib/actions';
+import { authenticate } from '../lib/actions';
 
 export default function LoginForm() {
-  const [errorMessage, dispatch] = useFormState(authentificate, undefined);
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
     <form action={dispatch} className="space-y-3">
