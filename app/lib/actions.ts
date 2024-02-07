ff2f23897acfa1ad2a4bda6234595fb2a87846ff
Shinji Pons <accounts@shinjipons.com>
2024-02-07 12:10:31 +0100

    Chapter 15 Adding the logout functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from 'next-auth/react';
+import { AuthError } from 'next-auth';
+
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({
@@ -103,4 +106,24 @@ export async function deleteInvoice(id: string) {
     } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }
+}
+
+export async function authentificate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData)
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+
 }
